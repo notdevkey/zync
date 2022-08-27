894ae5e9548b9839adfae0b1ab879105be0aa753
Ronalds Palacis <66126144+Devkeystuff@users.noreply.github.com>
2022-08-28 00:18:39 +0300

    Database seed (#2)

---
+++ prisma/seed.ts
@@ -0,0 +1,56 @@
+/**
+ * Adds seed data to your db
+ *
+ * @link https://www.prisma.io/docs/guides/database/seed-database
+ */
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const firstClassId = '5c03994c-fc16-47e0-bd02-d218a370a078';
+  await prisma.class.upsert({
+    where: {
+      id: firstClassId,
+    },
+    create: {
+      id: firstClassId,
+      name: 'Cat',
+      description: 'Cat is a domestic species of small carnivorous mammal',
+      properties: {
+        createMany: {
+          data: [
+            {
+              id: '1',
+              name: 'Name',
+              type: 'String',
+              description: 'The name of the cat',
+            },
+            {
+              id: '2',
+              name: 'Age',
+              type: 'Integer',
+              description: 'Age of the cat',
+            },
+            {
+              id: '3',
+              name: 'Breed',
+              type: 'String',
+              description: "The cat's breed",
+            },
+          ],
+        },
+      },
+    },
+    update: {},
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
