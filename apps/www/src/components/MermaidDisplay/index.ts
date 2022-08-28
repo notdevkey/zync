bc449939c149fd3fd6cf102879db34a3b3327622
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-08-28 20:46:42 +0300

    feat: added placeholder UML schema

---
+++ apps/www/src/components/MermaidDisplay/index.ts
@@ -0,0 +1,1 @@
+export { MermaidDisplay } from './mermaid-display.component';
--- apps/www/src/components/index.ts
+++ apps/www/src/components/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './Button';
 export { Form } from './Form';
 export { Input } from './Input';
+export { MermaidDisplay } from './MermaidDisplay';
 export { Select } from './Select';
