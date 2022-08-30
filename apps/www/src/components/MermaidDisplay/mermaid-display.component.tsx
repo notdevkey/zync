bff4d025b2f28920b0b3dd34fab3f1be92e80b83
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-08-30 22:57:47 +0300

    feat: displaying relations between classes

--- apps/www/src/components/MermaidDisplay/mermaid-display.component.tsx
+++ apps/www/src/components/MermaidDisplay/mermaid-display.component.tsx
@@ -1,3 +1,4 @@
+import { PrimitiveType } from '@/consts/enums';
 import { trpc } from '@/utils/trpc';
 import { Mermaid } from 'mdx-mermaid/lib/Mermaid';
 import { useCallback, useMemo } from 'react';
@@ -5,10 +6,13 @@ import { useCallback, useMemo } from 'react';
 export function MermaidDisplay() {
   const { data: classes } = trpc.useQuery(['class.all']);
 
+  // Generates a continuous string of classes formatted for mermaid display
   const generateClasses = useCallback(() => {
     if (!classes) return;
+
     const formattedClasses = classes.reduce(
       (prev, curr) =>
+        // Imagine prev as all previous values iteratively before curr in each loop
         prev +
         `
           class ${curr.name} {
@@ -22,10 +26,38 @@ export function MermaidDisplay() {
     return formattedClasses;
   }, [classes]);
 
+  /* Generates a continuous string of relations between classes
+  formatted for mermaid display */
+  const generateRelations = useCallback(() => {
+    if (!classes) return;
+
+    const relations = classes.reduce(
+      (prev, curr) =>
+        prev +
+        curr.properties
+          .map((p) => {
+            // If a the type if a primitive one, then we don't care about drawing relations for that
+            if (
+              !Object.values(PrimitiveType).includes(p.type as PrimitiveType)
+            ) {
+              // TODO: Generate different relation according to relation type
+              return `${curr.name} --> ${p.type}\n`;
+            }
+          })
+          .join(''),
+      '',
+    );
+    return relations;
+  }, [classes]);
+
   const formattedClasses = useMemo(() => {
     const classes = generateClasses();
-    return classes;
-  }, [generateClasses]);
+    const relations = generateRelations();
+
+    if (!classes || !relations) return;
+    // const relations = generateRelations();
+    return relations + '\n' + classes;
+  }, [generateClasses, generateRelations]);
 
   return (
     <Mermaid
