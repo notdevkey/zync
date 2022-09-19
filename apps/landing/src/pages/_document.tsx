e8414d68fb26ce84e41b1f9c632bc098939bf6c5
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-09-19 22:33:31 +0300

    chore: added local fonts to head

--- apps/landing/src/pages/_document.tsx
+++ apps/landing/src/pages/_document.tsx
@@ -8,6 +8,20 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="preload"
+          href="/fonts/ClashDisplay-Regular.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/ClashDisplay-Semibold.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
       </Head>
       <body>
         <Main />
