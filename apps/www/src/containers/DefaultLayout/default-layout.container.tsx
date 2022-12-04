a8d5b3ada9b5411b86035fbc779f3ad736863395
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-12-04 23:48:26 +0200

    feat: added favicon

--- apps/www/src/containers/DefaultLayout/default-layout.container.tsx
+++ apps/www/src/containers/DefaultLayout/default-layout.container.tsx
@@ -1,22 +1,32 @@
+import AxiosContextProvider from '@/contexts/axios.context';
 import Head from 'next/head';
 import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 type DefaultLayoutProps = { children: ReactNode };
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
 
 export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <>
       <Head>
-        <title>Prisma Starter</title>
+        <title>Zync</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="p-6">{children}</main>
-
-      {process.env.NODE_ENV !== 'production' && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      <QueryClientProvider client={queryClient}>
+        <AxiosContextProvider>
+          <main className="p-6">{children}</main>
+          {process.env.NODE_ENV !== 'production' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
+        </AxiosContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
